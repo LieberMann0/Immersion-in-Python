d46036507e8ee4ace54b742aca459eaded2377f1
Администратор <lieber.mann.0@gmail.com>
2023-03-22 10:55:42 +0300

    Fix Sem 3 task 4

--- Sem_03/task_04.py
+++ Sem_03/task_04.py
@@ -1,17 +1,11 @@
 # Создайте вручную список с повторяющимися элементами.
 # Удалите из него все элементы, которые встречаются дважды.
 
-#my_list = [1, 0, 4, 9, 5, 7, 0, 5, 0, 8, 1, 3, 9, 3, 4, 8, 8, 1, 9, 7, 5, 6, 9, 1, 3, 9, 5, 2]
-my_list = [1, 0, 4, 9, 5, 7, 0, 5, 0, 8, 1, 3, 9, 4, 1, 9, 5, 6, 9, 1, 9, 5, 2]
+my_list = [1, 0, 4, 9, 5, 7, 0, 5, 0, 8, 1, 3, 9, 3, 4, 8, 8, 1, 9, 7, 5, 6, 9, 1, 3, 9, 5, 2]
 
-for i in my_list:
+for i in set(my_list):
     if my_list.count(i) > 1:
         for item in range(my_list.count(i)):
             my_list.remove(i)
-
-# for i in my_list:
-#     if my_list.count(i) > 1:
-#         for item in range(my_list.count(i)):
-#             my_list.remove(i)
 
 print(my_list)
