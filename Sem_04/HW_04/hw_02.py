dac798e4d17e34e5f8df4886ba41236f6572ee61
Администратор <lieber.mann.0@gmail.com>
2023-03-27 09:22:37 +0300

    Add fix hw_02

--- Sem_04/HW_04/hw_02.py
+++ Sem_04/HW_04/hw_02.py
@@ -10,7 +10,7 @@ def swap_places_in_dictionary(**parameters):
         
 
 my_dict = {"Маша": 5, "Петя": 4, "Клава": 3, "Вовочка": 2}
-print(swap_places_in_dictionary(**my_dict))
+swap_places_in_dictionary(**my_dict)
 
 
 
