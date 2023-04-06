fd5e27d019c630321c8523769fd75d3e04b26288
Администратор <lieber.mann.0@gmail.com>
2023-04-06 23:58:01 +0300

    Add Sem 7 and HW 7

--- Sem_07/task_03.py
+++ Sem_07/task_03.py
@@ -11,8 +11,32 @@
 # ✔ При достижении конца более короткого файла,
 # возвращайтесь в его начало.
 
+def open_files(a:str, b:str) ->None:
+    with (
+        open(a, 'r', encoding='utf-8') as f1,
+        open(b, 'r', encoding='utf-8') as f2,
+        open('new_file.txt', 'w', encoding='utf-8') as f3
+        ):
+            lenf1 = sum(1 for i in f1)
+            lenf2 = sum(1 for i in f2)
+            print(lenf1, lenf2)
+            for i in range(max(lenf1, lenf2)):
+                text = f2.readline()
+                if text == '':
+                    f2.seek(0)
+                    text = f2.readline()
+                text = text[:-1]
+                num = f1.readline()
+                if num == '':
+                    f1.seek(0)
+                    num = f1.readline()
+                num = num.replace(' ', '')[:-1]
+                num1, num2 = num.split('|')
+
+                res_mult = int(num1) * float(num2)
+                if res_mult < 0:
+                    f3.write(f'{text.upper()}, {round(res_mult)}\n')
 
 
-
-
-
+if __name__ == '__main__':
+    open_files("text_file.txt", "data.txt")
