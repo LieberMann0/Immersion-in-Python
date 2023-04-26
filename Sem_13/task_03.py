8fa17b3df094686fda8ccd9d8de2674e27c5f7c9
Администратор <lieber.mann.0@gmail.com>
2023-04-26 17:24:49 +0300

    Add Seminar 13

---
+++ Sem_13/task_03.py
@@ -0,0 +1,16 @@
+# Создайте класс с базовым исключением и дочерние классы-исключения:
+# - ○ ошибка уровня,
+# - ○ ошибка доступа
+
+
+class BaseException(Exception):
+    pass
+
+
+class LevelException(BaseException):
+    print('Level')
+
+
+class AccesException(BaseException):
+    print('Access')
+    
